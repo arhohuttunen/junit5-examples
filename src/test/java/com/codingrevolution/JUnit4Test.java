0f171f50f8f88e2f01114c0efd6fbb4e76f45ad8
Arho Huttunen <arho.huttunen@iki.fi>
2017-11-16 11:39:24 +0200

    First version showing how to run both JUnit 4 and JUnit 5 tests.

---
+++ src/test/java/com/codingrevolution/JUnit4Test.java
@@ -0,0 +1,12 @@
+package com.codingrevolution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JUnit4Test {
+    @Test
+    public void vintageTest() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/codingrevolution/JUnit5Test.java
@@ -0,0 +1,12 @@
+package com.codingrevolution;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnit5Test {
+    @Test
+    public void jupiterTest() {
+        assertTrue(true);
+    }
+}

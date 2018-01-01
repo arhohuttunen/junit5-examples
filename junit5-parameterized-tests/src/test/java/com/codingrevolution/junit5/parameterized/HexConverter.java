411bf7e38f17280d34505afb6ed70e3498eab9b5
Arho Huttunen <arho.huttunen@iki.fi>
2018-01-01 17:25:19 +0200

    Check for conversion failure

--- junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/HexConverter.java
+++ junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/HexConverter.java
@@ -7,7 +7,12 @@ import org.junit.jupiter.params.converter.ArgumentConverter;
 class HexConverter implements ArgumentConverter {
 
     @Override
-    public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
-        return Integer.parseInt((String) source, 16);
+    public Object convert(Object source, ParameterContext context)
+            throws ArgumentConversionException {
+        try {
+            return Integer.parseInt((String) source, 16);
+        } catch (NumberFormatException e) {
+            throw new ArgumentConversionException("Cannot convert hex value", e);
+        }
     }
 }

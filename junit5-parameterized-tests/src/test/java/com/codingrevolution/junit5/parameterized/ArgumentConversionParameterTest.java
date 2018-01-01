1546bada48b39dac49f1b67b5c7bd2bd9193e8d3
Arho Huttunen <arho.huttunen@iki.fi>
2018-01-01 16:10:21 +0200

    Add a couple of more examples to demonstrate the implicit conversions

--- junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/ArgumentConversionParameterTest.java
+++ junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/ArgumentConversionParameterTest.java
@@ -3,9 +3,34 @@ package com.codingrevolution.junit5.parameterized;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.JavaTimeConversionPattern;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.concurrent.TimeUnit;
 
 public class ArgumentConversionParameterTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = { "SECONDS", "MINUTES" })
+    void convertStringToEnum(TimeUnit timeUnit) {
+        Assertions.assertNotNull(timeUnit);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "2018-01-01", "2018-01-31" })
+    void convertStringToLocalDate(LocalDate localDate) {
+        Assertions.assertEquals(Month.JANUARY, localDate.getMonth());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "01.01.2018", "31.01.2018" })
+    void convertStringToLocalDateWithConversionPattern(
+            @JavaTimeConversionPattern("dd.MM.yyyy") LocalDate localDate) {
+        Assertions.assertEquals(2018, localDate.getYear());
+    }
 
     @ParameterizedTest
     @CsvSource({

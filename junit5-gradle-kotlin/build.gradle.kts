2a2c688f3c805796bedaab2c3adc9556d5418686
Arho Huttunen <arho.huttunen@iki.fi>
2019-04-07 10:24:58 +0300

    Add an example of setting up JUnit Jupiter with Gradle Kotlin DSL

---
+++ junit5-gradle-kotlin/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ junit5-gradle-kotlin/src/test/kotlin/GradleKotlinExampleTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+class GradleKotlinExampleTest {
+
+    @Test
+    fun shouldRun() {
+
+    }
+}

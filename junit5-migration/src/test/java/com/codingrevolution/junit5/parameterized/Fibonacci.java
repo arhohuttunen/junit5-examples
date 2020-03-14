7ed4af8f4b4ca3c737130f5532ff3f159b33d205
Arho Huttunen <arho.huttunen@iki.fi>
2020-03-14 09:50:51 +0200

    Add a migration example about parameterized tests

---
+++ junit5-migration/src/test/java/com/codingrevolution/junit5/parameterized/Fibonacci.java
@@ -0,0 +1,15 @@
+package com.codingrevolution.junit5.parameterized;
+
+public class Fibonacci {
+    public static int compute(int n) {
+        int result;
+
+        if (n <= 1) {
+            result = n;
+        } else {
+            result = compute(n - 1) + compute(n - 2);
+        }
+
+        return result;
+    }
+}

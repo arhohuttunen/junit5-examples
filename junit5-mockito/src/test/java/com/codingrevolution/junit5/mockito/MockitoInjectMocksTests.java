90470fb1a7cea9d45b7f199049c0af79219b5429
Arho Huttunen <arho.huttunen@iki.fi>
2020-11-28 11:33:41 +0200

    Show that the @InjectMocks doesn't require MockitoExtension

--- junit5-mockito/src/test/java/com/codingrevolution/junit5/mockito/MockitoInjectMocksTests.java
+++ junit5-mockito/src/test/java/com/codingrevolution/junit5/mockito/MockitoInjectMocksTests.java
@@ -1,23 +1,34 @@
 package com.codingrevolution.junit5.mockito;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 public class MockitoInjectMocksTests {
 
     @Mock
     private OrderRepository orderRepository;
+    private AutoCloseable closeable;
     @InjectMocks
     private OrderService orderService;
+
+    @BeforeEach
+    void initService() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void closeService() throws Exception {
+        closeable.close();
+    }
 
     @Test
     void createOrderSetsTheCreationDate() {

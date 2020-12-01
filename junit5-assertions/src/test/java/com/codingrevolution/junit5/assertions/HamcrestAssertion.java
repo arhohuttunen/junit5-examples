87d4cd402d499d4511e66e1c96e3a56bdd1fda3d
Arho Huttunen <arho.huttunen@iki.fi>
2020-12-01 19:59:47 +0200

    Add very simplistic examples about Hamcrest, AssertJ and Truth

---
+++ junit5-assertions/src/test/java/com/codingrevolution/junit5/assertions/HamcrestAssertion.java
@@ -0,0 +1,19 @@
+package com.codingrevolution.junit5.assertions;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+class HamcrestAssertion {
+
+    @Test
+    void listHasOneItem() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        assertThat(list, hasSize(1));
+    }
+}

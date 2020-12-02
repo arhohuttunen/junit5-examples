7a1a84050c4b4628fcce527f5913e05834f9d889
Arho Huttunen <arho.huttunen@iki.fi>
2020-12-02 15:06:46 +0200

    Remove unused fields

--- junit5-mockito/src/main/java/com/codingrevolution/junit5/mockito/Order.java
+++ junit5-mockito/src/main/java/com/codingrevolution/junit5/mockito/Order.java
@@ -3,16 +3,7 @@ package com.codingrevolution.junit5.mockito;
 import java.time.LocalDateTime;
 
 public class Order {
-    private Long id;
     private LocalDateTime creationDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public LocalDateTime getCreationDate() {
         return creationDate;

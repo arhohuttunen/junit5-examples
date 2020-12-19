55007423a6a11be6c54d8c19fd49fc9b4102877f
Arho Huttunen <arho.huttunen@iki.fi>
2020-12-19 19:27:53 +0200

    Add an example for null and empty values in @CsvSource

--- junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/CsvSourceParameterTest.java
+++ junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/CsvSourceParameterTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CsvSourceParameterTest {
 
@@ -11,5 +13,29 @@ class CsvSourceParameterTest {
     @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "10, X"})
     void convertArabicToRomanNumeral(int arabic, String roman) {
         assertEquals(roman, new RomanNumeral(arabic).toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"John, "})
+    void nullArgument(String name, String address) {
+        assertNull(address);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"John, ''"})
+    void emptyArgument(String name, String address) {
+        assertTrue(address.isEmpty());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Jane, NULL"}, nullValues = "NULL")
+    void customNullArgument(String name, String address) {
+        assertNull(address);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"John, ''"}, emptyValue = "BLANK")
+    void emptyDefaultArgument(String task, String address) {
+        assertEquals("BLANK", address);
     }
 }

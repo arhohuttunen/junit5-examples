b8b07bbf16f0db5e51ee3c2a67c0fcb671145f02
Arho Huttunen <arho.huttunen@iki.fi>
2020-12-20 12:20:42 +0200

    Not everyone is familiar with roman numerals
    Add a task example that is also used in the aggregator examples, so it's familiar.

--- junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/CsvFileSourceParameterTest.java
+++ junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/CsvFileSourceParameterTest.java
@@ -1,11 +1,20 @@
 package com.codingrevolution.junit5.parameterized;
 
+import com.codingrevolution.junit5.parameterized.Task.Status;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CsvFileSourceParameterTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/tasks.csv", numLinesToSkip = 1)
+    void readTasks(String title, Status status, LocalDate date) {
+        System.out.printf("%s, %s, %s", title, status, date);
+    }
 
     @ParameterizedTest(name = "{index} => arabic={0}, roman={1}")
     @CsvFileSource(resources = "/roman-numeral.csv")
--- junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/CsvSourceParameterTest.java
+++ junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/CsvSourceParameterTest.java
@@ -1,7 +1,10 @@
 package com.codingrevolution.junit5.parameterized;
 
+import com.codingrevolution.junit5.parameterized.Task.Status;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -9,33 +12,43 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CsvSourceParameterTest {
 
+    @ParameterizedTest
+    @CsvSource({
+            "Write a blog post, IN_PROGRESS, 2020-12-20",
+            "Wash the car, OPENED, 2020-12-15"
+    })
+    void readTasks(String title, Status status, LocalDate date) {
+        System.out.printf("%s, %s, %s", title, status, date);
+    }
+
+    @ParameterizedTest
+    @CsvSource(", IN_PROGRESS, 2020-12-20")
+    void nullArgument(String title, Status status, LocalDate date) {
+        assertNull(title);
+    }
+
+    @ParameterizedTest
+    @CsvSource("'', IN_PROGRESS, 2020-12-20")
+    void emptyArgument(String title, Status status, LocalDate date) {
+        assertTrue(title.isEmpty());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = "NULL, IN_PROGRESS, 2020-12-20", nullValues = "NULL")
+    void customNullArgument(String title, Status status, LocalDate date) {
+        assertNull(title);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = "'', IN_PROGRESS, 2020-12-20", emptyValue = "BLANK")
+    void emptyDefaultArgument(String title, Status status, LocalDate date) {
+        assertEquals("BLANK", title);
+    }
+
     @ParameterizedTest(name = "{index} => arabic={0}, roman={1}")
     @CsvSource({"1, I", "2, II", "3, III", "4, IV", "5, V", "10, X"})
     void convertArabicToRomanNumeral(int arabic, String roman) {
         assertEquals(roman, new RomanNumeral(arabic).toString());
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"John, "})
-    void nullArgument(String name, String address) {
-        assertNull(address);
-    }
-
-    @ParameterizedTest
-    @CsvSource({"John, ''"})
-    void emptyArgument(String name, String address) {
-        assertTrue(address.isEmpty());
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"Jane, NULL"}, nullValues = "NULL")
-    void customNullArgument(String name, String address) {
-        assertNull(address);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"John, ''"}, emptyValue = "BLANK")
-    void emptyDefaultArgument(String task, String address) {
-        assertEquals("BLANK", address);
-    }
 }

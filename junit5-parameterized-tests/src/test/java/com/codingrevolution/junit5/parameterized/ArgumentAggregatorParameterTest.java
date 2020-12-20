4c4b1e1b42863f61d6c3abbcbc4dc16cd9c1dd3b
Arho Huttunen <arho.huttunen@iki.fi>
2020-12-20 09:13:26 +0200

    Add an arguments aggregator example

---
+++ junit5-parameterized-tests/src/test/java/com/codingrevolution/junit5/parameterized/ArgumentAggregatorParameterTest.java
@@ -0,0 +1,25 @@
+package com.codingrevolution.junit5.parameterized;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ArgumentAggregatorParameterTest {
+    @ParameterizedTest
+    @CsvSource({
+        "Write a blog post, IN_PROGRESS, 2020-12-20",
+        "Wash the car, OPENED, 2020-12-15"
+    })
+    void aggregateArgumentsWithAggregator(@AggregateWith(TaskAggregator.class) Task task) {
+        System.out.println(task);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Write a blog post, IN_PROGRESS, 2020-12-20",
+            "Wash the car, OPENED, 2020-12-15"
+    })
+    void aggregateArgumentsWithAnnotation(@CsvToTask Task task) {
+        System.out.println(task);
+    }
+}

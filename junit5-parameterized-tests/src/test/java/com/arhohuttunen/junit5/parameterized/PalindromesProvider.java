e777b1d4eb6a397ee7d6781e7c8f648918579936
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-02-12 17:00:03 +0200

    Replace deprecated API usage

--- junit5-parameterized-tests/src/test/java/com/arhohuttunen/junit5/parameterized/PalindromesProvider.java
+++ junit5-parameterized-tests/src/test/java/com/arhohuttunen/junit5/parameterized/PalindromesProvider.java
@@ -3,12 +3,13 @@ package com.arhohuttunen.junit5.parameterized;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.support.ParameterDeclarations;
 
 import java.util.stream.Stream;
 
 public class PalindromesProvider implements ArgumentsProvider {
     @Override
-    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+    public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameterDeclarations, ExtensionContext context) {
         return Stream.of("level", "madam", "saippuakivikauppias").map(Arguments::of);
     }
 }

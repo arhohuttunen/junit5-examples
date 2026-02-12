3c83a5a419bf8c0e0854d764728c9a65fd33b087
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-02-12 17:52:08 +0200

    Migrate to Spring Boot 4.0.2

--- junit5-nested-tests/src/test/java/com/arhohuttunen/junit5/ProductControllerWithoutNestedTest.java
+++ junit5-nested-tests/src/test/java/com/arhohuttunen/junit5/ProductControllerWithoutNestedTest.java
@@ -1,13 +1,13 @@
 package com.arhohuttunen.junit5;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 

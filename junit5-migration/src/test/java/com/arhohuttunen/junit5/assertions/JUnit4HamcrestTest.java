4e39a75b3624857ee077556b725edc6a21fae754
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-02-12 17:27:29 +0200

    Replace deprecated method usage

--- junit5-migration/src/test/java/com/arhohuttunen/junit5/assertions/JUnit4HamcrestTest.java
+++ junit5-migration/src/test/java/com/arhohuttunen/junit5/assertions/JUnit4HamcrestTest.java
@@ -1,16 +1,16 @@
 package com.arhohuttunen.junit5.assertions;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class JUnit4HamcrestTest {
 
     @Test
     public void numbersNotEqual() {
-        Assert.assertThat("numbers 1 and 2 are not equal", 1, is(not(equalTo(2))));
+        assertThat("numbers 1 and 2 are not equal", 1, is(not(equalTo(2))));
     }
 }

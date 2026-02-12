42ba4ebc69cfa843459e2cf0b44fde2136ce62b8
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-02-12 17:16:52 +0200

    Upgrade to Jetty 12

--- junit5-gradle-kotlin/build.gradle.kts
+++ junit5-gradle-kotlin/build.gradle.kts
@@ -16,9 +16,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.eclipse.jetty:jetty-servlet:11.0.26")
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-servlet:12.1.6")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    testImplementation("org.eclipse.jetty:jetty-server:11.0.26")
+    testImplementation("org.eclipse.jetty:jetty-server:12.1.6")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- junit5-gradle-kotlin/src/main/kotlin/com/arhohuttunen/junit5/kotlin/JettyServer.kt
+++ junit5-gradle-kotlin/src/main/kotlin/com/arhohuttunen/junit5/kotlin/JettyServer.kt
@@ -1,9 +1,9 @@
 package com.arhohuttunen.junit5.kotlin
 
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler
 import org.eclipse.jetty.server.Connector
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.ServerConnector
-import org.eclipse.jetty.servlet.ServletContextHandler
 import java.net.URI
 
 class JettyServer {

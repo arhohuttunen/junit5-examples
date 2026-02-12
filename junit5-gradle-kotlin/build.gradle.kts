3ec5f984da727c4b08c7dad968a6b5084f2da54a
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-02-12 17:16:52 +0200

    Update Jetty version

--- junit5-gradle-kotlin/build.gradle.kts
+++ junit5-gradle-kotlin/build.gradle.kts
@@ -16,9 +16,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.eclipse.jetty:jetty-servlet:11.0.12")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.26")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    testImplementation("org.eclipse.jetty:jetty-server:11.0.12")
+    testImplementation("org.eclipse.jetty:jetty-server:11.0.26")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
